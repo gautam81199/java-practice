fdb0753aad9baf7e57ebda2543a48682fa2a2e70
LNU <2160889@cognizant.com>
2024-08-28 10:12:32 +0530

    commit

---
+++ src/DataTypes/ExplicitCasting.java
@@ -0,0 +1,13 @@
+package DataTypes;
+
+public class ExplicitCasting {
+	public static void main(String[] args) {
+
+		
+		float measurement = 4.75f;
+		int recordedDepth = (int) measurement;
+		System.out.println(recordedDepth);
+
+	}
+
+}
---
+++ src/DataTypes/StringDataTypes.java
@@ -0,0 +1,11 @@
+package DataTypes;
+
+public class StringDataTypes {
+	public static void main(String[] args) {
+
+		String jokeOfTheDay = "Why was the computer cold? Because it left its Windows open!";
+		System.out.println(jokeOfTheDay);
+
+	}
+
+}
