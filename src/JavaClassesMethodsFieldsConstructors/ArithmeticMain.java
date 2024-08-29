9737bb2b85b8be5537af40ea1dab6f413f42efc9
LNU <2160889@cognizant.com>
2024-08-29 18:15:35 +0530

    commit

---
+++ src/JavaClassesMethodsFieldsConstructors/ArithmeticMain.java
@@ -0,0 +1,32 @@
+package JavaClassesMethodsFieldsConstructors;
+
+public class ArithmeticMain {
+	double piValue = 3.14;
+    static final double PI_VALUE = 3.14;
+
+    public static void main(String[] args) {
+    	ArithmeticMain arithmeticmain = new ArithmeticMain();
+        int result = arithmeticmain.sum(20, 5);
+        System.out.println(result);
+    }
+
+    public int sum(int num1, int num2) {
+        subtract(num1, num2);
+        addition(num1, num2);
+        int result = num1 + num2;
+        return result;
+    }
+
+    public int subtract(int num1, int num2) {
+        int result = num1 - num2;
+        return result;
+    }
+
+    public static int addition(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+
+
+
+}
