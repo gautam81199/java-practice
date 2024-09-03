b90dacf4274a267b2381a1c233af03ddc6d3ec36
LNU <2160889@cognizant.com>
2024-09-03 17:59:49 +0530

    commit

---
+++ src/String/stringConcatenate.java
@@ -0,0 +1,18 @@
+package String;
+
+public class stringConcatenate {
+	
+	public static void main(String[] args) {
+        // Step 1: Define the strings to be concatenated
+        String greeting = "Hello ";
+        String name = "Alex ";
+        String message = "Welcome to the elite Java Club!";
+        
+        // Step 2: Concatenate the strings into one coherent message
+        String personalizedMessage = greeting + name + message;
+        
+        // Step 3: Print the concatenated message to the console
+        System.out.println(personalizedMessage);
+    }
+
+}
